0e168e380f25ece709d5a26b256d8a9f060b9c5d
lizethcas <64451121+lizethcas@users.noreply.github.com>
2021-10-06 18:28:46 -0500

    Creando servidor con NodeJS

--- Backend_Nodjes/index.js
+++ Backend_Nodjes/index.js
@@ -1,10 +1,16 @@
 
  'use strict'
  var mongoose = require("mongoose");
+ var app = require('./app');
+ var port = 3700;
  mongoose.Promise = global.Promise;
  mongoose.connect("mongodb://localhost:27017/portafolio ")
            .then(() => {
               console.log("conexion a base de datos establecida con exito..");
+
+              app.listen(port, () => {
+                console.log("Servidor corriendo correctamente con NodeJS");
+              });
                 })
            .catch(err => console.log(err));
 
